bbd51fe1c250bd264aa2446e44ae3ac6c4a6fed0
Rush Frisby <rush.frisby@derivesystems.com>
2018-06-27 10:52:31 -0400

    Add content type helper

---
+++ Serializer.NET/ContentType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Serializer.NET
+{
+	public class ContentType
+	{
+		public const string Json = "application/json";
+		public const string Protobuf = "application/protobuf";
+
+		private static IDictionary<string, Type> _mapping = GetDefaultSerializerMapping();
+
+		public static IDictionary<string, Type> GetDefaultSerializerMapping()
+		{
+			return new Dictionary<string, Type>()
+			{
+				{ Json, typeof(JsonSerializer) },
+				{ Protobuf, typeof(ProtobufSerializer) },
+			};
+		}
+
+		public static void SetSerializerMapping(IDictionary<string, Type> mapping)
+		{
+			_mapping = mapping ?? throw new ArgumentNullException(nameof(mapping));
+		}
+
+		public static ISerializer GetSerializerFor(string contentType)
+		{
+			if(!_mapping.ContainsKey(contentType))
+			{
+				return null;
+			}
+			return (ISerializer)Activator.CreateInstance(_mapping[contentType]);
+		}
+	}
+}
