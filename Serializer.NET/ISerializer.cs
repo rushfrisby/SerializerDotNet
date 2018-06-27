2732891c0c11a41a15aa88a876ba128b1c7c7de4
Rush Frisby <rush.frisby@derivesystems.com>
2018-06-27 10:25:31 -0400

    Add initial serializer implementations

---
+++ Serializer.NET/ISerializer.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Serializer.NET
+{
+    public interface ISerializer
+    {
+        void Serialize<T>(T instance, Stream destination);
+
+        Task SerializeAsync<T>(T instance, Stream destination);
+
+        byte[] Serialize<T>(T instance);
+
+        Task<byte[]> SerializeAsync<T>(T instance);
+
+        T Deserialize<T>(Stream source);
+
+        Task<T> DeserializeAsync<T>(Stream source);
+
+        T Deserialize<T>(byte[] source);
+
+        Task<T> DeserializeAsync<T>(byte[] source);
+    }
+}
---
+++ Serializer.NET/JsonSerializer.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+
+namespace Serializer.NET
+{
+    public class JsonSerializer : ISerializer
+    {
+        public void Serialize<T>(T instance, Stream destination)
+        {
+            var data = Serialize(instance);
+            destination.Write(data, 0, data.Length);
+        }
+        public async Task SerializeAsync<T>(T instance, Stream destination)
+        {
+            await Task.Factory.StartNew(() => Serialize(instance, destination));
+        }
+
+        public byte[] Serialize<T>(T instance)
+        {
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(instance));
+        }
+
+        public async Task<byte[]> SerializeAsync<T>(T instance)
+        {
+            return await Task.Factory.StartNew(() => Serialize(instance));
+        }
+
+        public T Deserialize<T>(Stream source)
+        {
+            using (var r = new StreamReader(source, Encoding.UTF8))
+            {
+                return JsonConvert.DeserializeObject<T>(r.ReadToEnd());
+            }
+        }
+
+        public async Task<T> DeserializeAsync<T>(Stream source)
+        {
+            return await Task.Factory.StartNew(() => Deserialize<T>(source));
+        }
+
+        public T Deserialize<T>(byte[] source)
+        {
+            using (var ms = new MemoryStream(source))
+            {
+                return Deserialize<T>(ms);
+            }
+        }
+
+        public async Task<T> DeserializeAsync<T>(byte[] source)
+        {
+            return await Task.Factory.StartNew(() => Deserialize<T>(source));
+        }
+    }
+}
